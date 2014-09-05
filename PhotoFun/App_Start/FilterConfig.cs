2254e11e513b89b3bb382f2a9c200964a7503feb
xxdomxx <dominic.v@outlook.com>
2014-09-05 14:57:16 -0400

    PhotoFun CommitINI

---
+++ PhotoFun/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoFun
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
