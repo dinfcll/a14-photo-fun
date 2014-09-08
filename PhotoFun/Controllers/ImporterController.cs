3ac054261ccfb267426e6ac123e6997fd4293921
1234901 <nic_pandolfo@hotmail.com>
2014-09-08 13:42:04 -0400

    Page importer enregistre l'image dans App_Data

---
+++ PhotoFun/Controllers/ImporterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+namespace PhotoFun.Controllers
+{
+    public class ImporterController : Controller
+    {
+        //
+        // GET: /Importer/
+
+        public ActionResult UploadDocument()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("/App_Data/"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+
+            return RedirectToAction("UploadDocument");
+        }
+    }
+}
+
