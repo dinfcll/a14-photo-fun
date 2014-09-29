01a19aab8efdf804185e3e442034badf5fcaba8e
Nicolas Pandolfo-Alary <nic_pandolfo@hotmail.com>
2014-09-29 12:25:21 -0400

    Retour Diner

--- PhotoFun/Models/PhotoFunBD.cs
+++ PhotoFun/Models/PhotoFunBD.cs
@@ -9,20 +9,23 @@ namespace PhotoFun.Models
 {
     public class PhotoFunBD
     {
-        SqlConnection conn = new SqlConnection("ConnectionMaBD");
+        
         public bool InsererUtil(RegisterModel model)
         {
             bool resultat;
             try
             {
+                SqlConnection conn = new SqlConnection("ConnectionMaBD");
                 SqlCommand scAjouter = new SqlCommand("Insert into Utilisateur values(NumUtil.NextVal" + model.UserName + ", " + model.Password + ", "
-                    +model.Courriel + ", " + model.PrenomUtil + ", " + model.NomUtil + ");");
-                scAjouter.BeginExecuteNonQuery();
-                resultat=true;
+                    + model.Courriel + ", " + model.PrenomUtil + ", " + model.NomUtil + ");");
+                scAjouter.Connection.Open();
+                scAjouter.ExecuteNonQuery();
+                resultat = true;
+
             }
             catch
             {
-                resultat=false;
+                resultat = false;
             }
             return resultat;
         }
