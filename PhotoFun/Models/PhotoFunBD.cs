c9db5e3f98bbd079226c9fef374b83e84227a726
Nicolas Pandolfo-Alary <nic_pandolfo@hotmail.com>
2014-10-03 16:08:03 -0400

    Insert into fonctionnel

--- PhotoFun/Models/PhotoFunBD.cs
+++ PhotoFun/Models/PhotoFunBD.cs
@@ -9,19 +9,24 @@ namespace PhotoFun.Models
 {
     public class PhotoFunBD
     {
-        
+
         public bool InsererUtil(RegisterModel model)
         {
             bool resultat;
-            string cs ="Data Source=G264-09/SQLEXPRESS;Initial Catalog=tempdb;Integrated Security=True";
+            string cs = "Data Source=G264-09\\SQLEXPRESS;Initial Catalog=tempdb;Integrated Security=True";
             using (SqlConnection conn = new SqlConnection(cs))
             {
                 try
                 {
-                    SqlCommand scAjouter = new SqlCommand("Insert into Utilisateur values (NumUtil.NextVal, " + model.UserName + ", " + model.Password + ", "
-                        + model.Courriel + ", " + model.PrenomUtil + ", " + model.NomUtil + ");", conn);
                     conn.Open();
-                    scAjouter.ExecuteReader();
+                    SqlCommand scAjouter = new SqlCommand("Insert into Utilisateur (NumUtil,IDUtil,MotPasse,CourrielUtil,PrenomUtil,NomUtil) values (100,'"
+                        + model.UserName + "', '" + model.Password + "', '" + model.Courriel + "', '" + model.PrenomUtil + "', '" + model.NomUtil + "');", conn);
+                    scAjouter.Parameters.AddWithValue("IDUtil", model.UserName);
+                    scAjouter.Parameters.AddWithValue("MotPasse", model.Password);
+                    scAjouter.Parameters.AddWithValue("CourrielUtil", model.Courriel);
+                    scAjouter.Parameters.AddWithValue("PrenomUtil", model.PrenomUtil);
+                    scAjouter.Parameters.AddWithValue("NomUtil", model.NomUtil);
+                    scAjouter.ExecuteNonQuery();
                     conn.Close();
                     resultat = true;
 
