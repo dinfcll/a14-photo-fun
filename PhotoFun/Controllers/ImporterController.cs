1d232b9bc365e7a733c5e2dd5b291225ed6cebd0
xxdomxx <dominic.v@outlook.com>
2014-10-03 16:29:48 -0400

    Barre de navigation bootstrap marche!!
    Yeahh!!

--- PhotoFun/Controllers/ImporterController.cs
+++ PhotoFun/Controllers/ImporterController.cs
@@ -22,31 +22,31 @@ namespace PhotoFun.Controllers
         [HttpPost]
         public ActionResult Upload()
         {
-            FileStream fs = new FileStream("E:/Implantation d'un système informatique/a14-photo-fun/PhotoFun/Content/FichierText/NomPhoto.txt", FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-            StreamWriter sw = new StreamWriter(fs);
-            string contenuFichier = "";
+            //FileStream fs = new FileStream("E:/Implantation d'un système informatique/a14-photo-fun/PhotoFun/Content/FichierText/NomPhoto.txt", FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            //StreamWriter sw = new StreamWriter(fs);
+            //string contenuFichier = "";
            
            
-            if (Request.Files.Count > 0)
-            {
+            //if (Request.Files.Count > 0)
+            //{
                 
-                var file = Request.Files[0];
+            //    var file = Request.Files[0];
 
-                if (file != null && file.ContentLength > 0)
-                {
-                    var fileName = Path.GetFileName(file.FileName);
-                    var path = Path.Combine(Server.MapPath("/Content/Photo"), fileName);
-                    file.SaveAs(path);
-                    //ajouter le fichier dans un fichier text
-                    contenuFichier = fileName+";";
-                    Thread.Sleep(10);
-                    fs.Seek(0, SeekOrigin.End);
-                    sw.Write(contenuFichier);
+            //    if (file != null && file.ContentLength > 0)
+            //    {
+            //        var fileName = Path.GetFileName(file.FileName);
+            //        var path = Path.Combine(Server.MapPath("/Content/Photo"), fileName);
+            //        file.SaveAs(path);
+            //        //ajouter le fichier dans un fichier text
+            //        contenuFichier = fileName+";";
+            //        Thread.Sleep(10);
+            //        fs.Seek(0, SeekOrigin.End);
+            //        sw.Write(contenuFichier);
                               
-                }
-            }
-            sw.Close();
-            fs.Close();
+            //    }
+            //}
+            //sw.Close();
+            //fs.Close();
             
             
             return RedirectToAction("UploadDocument");
