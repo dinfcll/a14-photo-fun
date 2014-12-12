9f0fb2ee991ca7eeef8df9350e696dc9ddf6ddef
Nicolas Pandolfo-Alary <nic_pandolfo@hotmail.com>
2014-12-12 13:58:28 -0500

    ProfilModel ReSharper

--- PhotoFun/Models/ProfilModel.cs
+++ PhotoFun/Models/ProfilModel.cs
@@ -1,99 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace PhotoFun.Models
+﻿namespace PhotoFun.Models
 {
     public class ProfilModel
     {
-        private string m_IdUtilRechercher;
-        private int m_nbAbonnement;
-        private bool m_Abonner;
-        private string m_Courriel;
-        private string m_NomUtil;
-        private string m_PrenomUtil;
-
         public ProfilModel()
         {
-            m_IdUtilRechercher = null;
-            m_nbAbonnement = 0;
-            m_Abonner = false;
-            m_Courriel = null;
-            m_NomUtil = null;
-            m_PrenomUtil = null;
+            IdUtilRechercher = null;
+            NbAbonnement = 0;
+            Abonner = false;
+            Courriel = null;
+            NomUtil = null;
+            PrenomUtil = null;
         }
 
         
-        public string Courriel
-        { 
-            get
-            {
-                return m_Courriel;
-            }
-            set
-            {
-                m_Courriel = value;
-            } 
-        }
+        public string Courriel { get; set; }
 
-        public string NomUtil
-        {
-            get 
-            {
-                return m_NomUtil;
-            }
-            set 
-            {
-                m_NomUtil = value;
-            } 
-        }
-        public string PrenomUtil
-        {
-            get 
-            {
-                return m_PrenomUtil;
-            }
-            set 
-            {
-                m_PrenomUtil = value;
-            }
-        }
+        public string NomUtil { get; set; }
 
-        public string IdUtilRechercher
-        {
-            get
-            {
-                return m_IdUtilRechercher;
-            }
-            set
-            {
-                m_IdUtilRechercher = value;
-            }
-        }
+        public string PrenomUtil { get; set; }
 
-        public int NbAbonnement
-        {
-            get
-            {
-                return m_nbAbonnement;
-            }
-            set
-            {
-                m_nbAbonnement = value;
-            }
-        }
+        public string IdUtilRechercher { get; set; }
 
-        public bool Abonner
-        {
-            get
-            {
-                return m_Abonner;
-            }
-            set
-            {
-                m_Abonner = value;
-            }
-        }
+        public int NbAbonnement { get; set; }
+
+        public bool Abonner { get; set; }
     }
 }
